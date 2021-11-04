536b2c68ea7d30147b145fa96b94c7cd6c662409
yuki yoshinari <seal0828.4477@gmail.com>
2021-11-04 17:24:22 +0900

    initia commit

---
+++ getproperty.ps1
@@ -0,0 +1,22 @@
+echo "getproperty.ps1 executing.."
+# show file information
+# Get-ItemProperty $path | Format-List -property *
+
+# define directory of tagged data
+$path = Join-Path (Convert-Path .) ("data")
+
+# get all tags from each file in 'data' dir
+$shellobj = New-Object -COMObject Shell.Application
+$folder = $shellobj.NameSpace($path)
+$csv = @()
+$row = New-Object PSObject | Select-Object tag, path
+foreach($f in Get-Item "data\*"){
+    $fileobj = $folder.ParseName($f.Name)
+    $row.tag = $folder.GetDetailsOf($fileobj, 18) # tag detail is the 18th property
+    $row.path = Resolve-Path $f
+    $csv += $row
+}
+
+# save csv as 'tag.csv'
+$csv | ft -AutoSize
+$csv | ConvertTo-Csv -NoTypeInformation | Select-Object -skip 1 | Set-Content -path tag.csv -Encoding UTF8
---
+++ ps1exe.bat
@@ -0,0 +1,1 @@
+PowerShell -ExecutionPolicy RemoteSigned .\getproperty.ps1
